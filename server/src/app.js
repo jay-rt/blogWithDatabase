01faa6bb1d1ece85c0f478d78f3d9b6f0efd60e0
jay-rt <thapa.jayrt@gmail.com>
2022-08-26 13:58:00 -0400

    made production version of react app and made it so everything can be served from backend

--- server/src/app.js
+++ server/src/app.js
@@ -2,13 +2,21 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import postRouter from "./routes/post.js";
+import postRouter from "../routes/post.js";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+//getting path to directory
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 //Creating a mongodb connection
 mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
 
 //Creating a new instance of express
 const app = express();
+
+//serving static files
+app.use(express.static(join(__dirname, "build")));
 
 // configuring cors
 const corsOption = {
@@ -19,6 +27,10 @@ app.use(cors(corsOption));
 
 app.use(express.json());
 app.use("/posts", postRouter);
+
+app.use("*", (req, res) => {
+  res.sendFile(join(__dirname, "build", "index.html"));
+});
 
 //Listening for connection at specific port
 app.listen(3000, () => {
